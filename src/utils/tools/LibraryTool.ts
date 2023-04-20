7c74308be882b02de40bbf0ee587f9ed120c4a13
0xtito <welovethepenguins@gmail.com>
2023-04-20 18:12:14 -0400

    init custom tools

---
+++ src/utils/tools/LibraryTool.ts
@@ -0,0 +1,66 @@
+import type { CallbackManager } from "langchain/callbacks";
+import { BaseLanguageModel } from "langchain/dist/base_language";
+import { OpenAIEmbeddings } from "langchain/embeddings";
+import { Tool } from "langchain/tools";
+import { SupabaseVectorStore } from "langchain/vectorstores";
+
+import { supabase } from "../clients/supabase-client";
+import { StringPromptValue } from "langchain/prompts";
+
+interface LibraryToolArgs {
+  model: BaseLanguageModel;
+  philosopher: string;
+  embeddings: OpenAIEmbeddings;
+  verbose?: boolean;
+  callbackManager?: CallbackManager;
+}
+
+export class LibraryTool extends Tool {
+  private model: BaseLanguageModel;
+  private embeddings: OpenAIEmbeddings;
+  private philosopher: string;
+
+  constructor({
+    model,
+    philosopher,
+    embeddings,
+    verbose,
+    callbackManager,
+  }: LibraryToolArgs) {
+    super(verbose, callbackManager);
+    this.model = model;
+    this.philosopher = philosopher;
+    this.embeddings = embeddings;
+  }
+
+  name = "Library Tool";
+
+  description = `Search through your Library to remember your theories, stances, and beliefs. Your library is in the form of vectorstores. The vectorstore contains your writings. `;
+
+  async _call(text: string) {
+    const vs = await SupabaseVectorStore.fromExistingIndex(this.embeddings, {
+      client: supabase,
+      tableName: `vitalik_essays`,
+      queryName: "match_documents",
+    });
+
+    const rawResults = await vs.similaritySearch(text, 5);
+
+    const results = rawResults.map((doc) => doc.pageContent).join("\n");
+
+    const input = `You are ${this.philosopher
+      .split("_")
+      .map((name) => name[0].toUpperCase() + name.slice(1))
+      .join(
+        " "
+      )} and this is your work. Write a descriptive summary of the following context:\n\n
+        ${results}
+        \n\nConcise Summary: 
+        `;
+
+    const ans = await this.model.generatePrompt([new StringPromptValue(input)]);
+    console.log(ans.generations[0][0].text);
+    return ans.generations[0][0].text;
+    return `Final answer: ${ans.generations[0][0].text}`;
+  }
+}
